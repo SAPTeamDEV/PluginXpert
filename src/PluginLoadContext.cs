bc08eb620239670c204d58c1707f677dd9c55d4d
Alireza Poodineh <itsaeliux@gmail.com>
2023-08-20 20:53:49 +0330

    Add PluginManager

---
+++ src/PluginLoadContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace SAPTeam.PluginXpert;
+
+public class PluginLoadContext : AssemblyLoadContext
+{
+    private readonly AssemblyDependencyResolver resolver;
+
+    public PluginLoadContext(string pluginPath)
+    {
+        resolver = new AssemblyDependencyResolver(pluginPath);
+    }
+
+    protected override Assembly Load(AssemblyName assemblyName)
+    {
+        string assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
+        if (assemblyPath != null)
+        {
+            return LoadFromAssemblyPath(assemblyPath);
+        }
+
+#pragma warning disable CS8603 // Possible null reference return.
+        return null;
+#pragma warning restore CS8603 // Possible null reference return.
+    }
+
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+    {
+        string libraryPath = resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+        return libraryPath != null ? LoadUnmanagedDllFromPath(libraryPath) : IntPtr.Zero;
+    }
+}
---
+++ src/PluginManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace SAPTeam.PluginXpert;
+
+public class PluginManager
+{
+    public static List<T> GetPlugins<T>(string directory)
+        where T : new()
+    {
+        IEnumerable<T> commands = Directory.EnumerateFiles(Path.Join(AppContext.BaseDirectory, "Plugins"), "*.dll").SelectMany(pluginPath =>
+        {
+            Assembly pluginAssembly = LoadPlugin(pluginPath);
+            return CreateCommands<T>(pluginAssembly);
+        }).ToList();
+
+        return (List<T>)commands;
+    }
+
+    private static Assembly LoadPlugin(string pluginLocation)
+    {
+        PluginLoadContext loadContext = new(pluginLocation);
+        return loadContext.LoadFromAssemblyName(AssemblyName.GetAssemblyName(pluginLocation));
+    }
+
+    private static IEnumerable<T> CreateCommands<T>(Assembly assembly)
+    {
+        int count = 0;
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            if (typeof(T).IsAssignableFrom(type))
+            {
+                if (Activator.CreateInstance(type) is T result)
+                {
+                    count++;
+                    yield return result;
+                }
+            }
+        }
+
+        if (count == 0)
+        {
+            string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+            throw new ApplicationException(
+                $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                $"Available types: {availableTypes}");
+        }
+    }
+}
