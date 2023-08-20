15a3ac3d8c7cfaddd4d2720e5779ff28445b5993
Alireza Poodineh <itsaeliux@gmail.com>
2023-08-20 22:00:54 +0330

    Add contructor to PluginManager

--- src/PluginManager.cs
+++ src/PluginManager.cs
@@ -3,19 +3,36 @@
 namespace SAPTeam.PluginXpert;
 
 /// <summary>
-/// Represents static methods for loading generic plugins.
+/// Represents methods for loading generic or managed plugins.
 /// </summary>
-public static class PluginManager
+/// <typeparam name="T">A class that implements <see cref="IPlugin"/> and has a parameterless constructor.</typeparam>
+public class PluginManager<T>
+    where T : IPlugin, new()
 {
+    /// <summary>
+    /// Gets the list of loaded plugins.
+    /// </summary>
+    public List<T> Plugins { get; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="PluginManager{T}"/> and loads plugins.
+    /// </summary>
+    /// <param name="directory">Directory of plugin assemblies.</param>
+    /// <param name="namePattern">A regex pattern for selecting plugin assemblies.</param>
+    public PluginManager(string directory, string namePattern = "*.dll")
+    {
+        Plugins = GetPlugins<T>(directory, namePattern);
+    }
+
     /// <summary>
     /// Loads all plugins with the given <typeparamref name="T"/> type.
     /// </summary>
-    /// <typeparam name="T">Type of objects that would be loaded.</typeparam>
+    /// <typeparam name="TPlugin">Type of objects that would be loaded.</typeparam>
     /// <param name="directory">Directory of plugin assemblies.</param>
     /// <param name="namePattern">A regex pattern for selecting plugin assemblies.</param>
     /// <returns></returns>
-    public static List<T> GetPlugins<T>(string directory, string namePattern = "*.dll")
-        where T : new()
+    public static List<TPlugin> GetPlugins<TPlugin>(string directory, string namePattern = "*.dll")
+        where TPlugin : new()
     {
         IEnumerable<T> commands = Directory.EnumerateFiles(directory, namePattern).SelectMany(pluginPath =>
         {
@@ -23,7 +40,7 @@ public static class PluginManager
             return CreateCommands<T>(pluginAssembly);
         }).ToList();
 
-        return (List<T>)commands;
+        return (List<TPlugin>)commands;
     }
 
     private static Assembly LoadPlugin(string pluginLocation)
@@ -32,15 +49,15 @@ public static class PluginManager
         return loadContext.LoadFromAssemblyName(AssemblyName.GetAssemblyName(pluginLocation));
     }
 
-    private static IEnumerable<T> CreateCommands<T>(Assembly assembly)
+    private static IEnumerable<TPlugin> CreateCommands<TPlugin>(Assembly assembly)
     {
         int count = 0;
 
         foreach (Type type in assembly.GetTypes())
         {
-            if (typeof(T).IsAssignableFrom(type))
+            if (typeof(TPlugin).IsAssignableFrom(type))
             {
-                if (Activator.CreateInstance(type) is T result)
+                if (Activator.CreateInstance(type) is TPlugin result)
                 {
                     count++;
                     yield return result;
