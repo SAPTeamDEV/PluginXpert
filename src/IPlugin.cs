11aff40307a51f6689385e0680e66ee6fd9e8ab6
Alireza Poodineh <itsaeliux@gmail.com>
2023-08-20 21:42:03 +0330

    Add early IPlugin interface

---
+++ src/IPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPTeam.PluginXpert
+{
+    /// <summary>
+    /// Provides standard interface to implement managed plugin.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Gets the name of this plugin.
+        /// </summary>
+        string Name { get; }
+    }
+}
