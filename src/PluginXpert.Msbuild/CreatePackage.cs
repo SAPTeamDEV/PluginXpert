851da0054b2fcf4af8c62dc7d316db7d611dba96
Alireza Poodineh <itsaeliux@gmail.com>
2025-03-22 23:32:26 +0330

    Initially add the msbuild task

---
+++ src/PluginXpert.Msbuild/CreatePackage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace PluginXpert.Msbuild
+{
+    public class CreatePackage : Task
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Output { get; set; }
+
+        [Required]
+        public string BuildOutput { get; set; }
+
+        public string Name { get; set; }
+
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+}
